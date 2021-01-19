e48aec9c0a569f12c4cc89a7a16a24e7f6b1512c
Felix Awortwe <felixawortwe14@gmail.com>
2021-01-18 18:11:00 -0800

    first commit

--- work_trials/trial.js
+++ work_trials/trial.js
@@ -3,3 +3,7 @@
 var a = 10; //for global variables
 const b = 'Hello';
 let c = true; //for limited scope
+
+const d = "felix"; //Can never be changed , it is constant
+
+console.log(d); // it is used ti display outputs
